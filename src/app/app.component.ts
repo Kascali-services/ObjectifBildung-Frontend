29928df21c8a6a92122115674670ada6b26ea353
Kascabrel <107616396+Kascabrel@users.noreply.github.com>
2025-10-06 08:42:51 +0200

    Feature: styling Add: bootstrap style to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavBarComponent} from './shared/components/nav-bar/nav-bar.component';
+import {FooterBarComponent} from './shared/components/footer-bar/footer-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, FooterBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
