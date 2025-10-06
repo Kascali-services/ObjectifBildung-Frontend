6f115e809170c758166cca30a9565e771c09ad26
Kascabrel <107616396+Kascabrel@users.noreply.github.com>
2025-10-06 09:59:51 +0200

    Feature: language Add: change language on the footer page

--- src/app/shared/components/footer-bar/footer-bar.component.ts
+++ src/app/shared/components/footer-bar/footer-bar.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-footer-bar',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './footer-bar.component.html',
   styleUrl: './footer-bar.component.scss'
 })
 export class FooterBarComponent {
+  language = 'fr';
+  translations: any = {};
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.loadLanguage(this.language);
+  }
+
+  loadLanguage(lang: string): void {
+    this.http.get(`assets/language/${lang}.json`).subscribe({
+      next: (data) => (this.translations = data),
+      error: () => console.error(`Erreur lors du chargement de la langue : ${lang}`)
+    });
+  }
+
+  changeLanguage(lang: string): void {
+    this.language = lang;
+    this.loadLanguage(lang);
+  }
 
 }
