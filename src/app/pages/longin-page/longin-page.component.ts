d3cdade19d6c577a525fa77f2a6fee459f62a185
Kascabrel <107616396+Kascabrel@users.noreply.github.com>
2025-10-06 15:36:02 +0200

    Feature: Authentication Add: logging component

---
+++ src/app/pages/longin-page/longin-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-longin-page',
+  imports: [],
+  templateUrl: './longin-page.component.html',
+  styleUrl: './longin-page.component.scss'
+})
+export class LonginPageComponent {
+
+}
