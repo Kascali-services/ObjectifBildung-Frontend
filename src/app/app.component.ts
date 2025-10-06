5c2fbe40216ee56925c557526df3d74de2f994aa
Kascabrel <107616396+Kascabrel@users.noreply.github.com>
2025-10-06 14:37:00 +0200

    Feature: language Add: change language on the navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NavBarComponent} from './shared/components/nav-bar/nav-bar.component';
 import {FooterBarComponent} from './shared/components/footer-bar/footer-bar.component';
+import {NavbarComponent} from './shared/components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent, FooterBarComponent],
+  imports: [RouterOutlet, FooterBarComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent {
+export class NavbarComponent implements OnInit {
+  isLogged = true;
+  currentLang = 'fr';
+  translations: {
+    nav_about?: string;
+    nav_services?: string;
+    nav_translate?: string;
+    nav_cv?: string;
+    nav_dashboard?: string;
+  } = {};
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadLanguage(this.currentLang);
+  }
+
+  async loadLanguage(lang: string): Promise<void> {
+    try {
+      const data = await firstValueFrom(
+        this.http.get<typeof this.translations>(`assets/language/${lang}.json`)
+      );
+      this.translations = data;
+      this.currentLang = lang;
+    } catch (error) {
+      console.error('Erreur lors du chargement de la langue :', lang);
+    }
+  }
+
+  changeLang(lang: string): void {
+    this.loadLanguage(lang);
+  }
+
+  toggleLogin(): void {
+    this.isLogged = !this.isLogged;
+  }
 }
